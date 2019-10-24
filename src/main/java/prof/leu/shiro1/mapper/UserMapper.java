444134c2f36e1abcdee5d532e43fd2531d50a2d3
liuhongshuo <2624886248@qq.com>
2019-10-24 09:38:02 +0800

    a

--- src/main/java/prof/leu/shiro1/mapper/UserMapper.java
+++ src/main/java/prof/leu/shiro1/mapper/UserMapper.java
@@ -1,7 +1,9 @@
 package prof.leu.shiro1.mapper;
 
+import org.springframework.stereotype.Service;
 import prof.leu.shiro1.domain.User;
-
+@Service
 public interface UserMapper {
      User findByName(String name) ;
+     User findById(Integer id) ;
 }
